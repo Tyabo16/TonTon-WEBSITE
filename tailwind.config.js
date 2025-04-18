222e0d7ac9bcb988b445ee22da0e130d1d30dc47
Tyabo16 <younesdjeha0@gmail.com>
2025-04-18 01:31:24 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: { 
+        primary: "#FF0000", 
+        secondary: "#FFFFFF" 
+      },
+      borderRadius: {
+        none: "0px",
+        sm: "4px",
+        DEFAULT: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "32px",
+        full: "9999px",
+        button: "8px",
+      },
+    },
+  },
+} 
